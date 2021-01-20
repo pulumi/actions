64e9686186f6ae566084ec2616063c913b41baf6
Simen A. W. Olsen <so@bjerk.io>
2021-01-20 22:42:18 +0100

    feat: Add basic implementation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
-import { makeConfig } from './config';
+import { makeConfig, Commands } from './config';
 import * as pulumiCli from './libs/pulumi-cli';
 import { invariant } from './libs/utils';
+import { LocalWorkspace } from '@pulumi/pulumi/x/automation';
+import * as core from '@actions/core';
 
 (async () => {
   const config = await makeConfig();
 
   invariant(pulumiCli.isAvailable(), 'Pulumi CLI is not available.');
 
-  console.log(config);
+  const stack = await LocalWorkspace.selectStack({
+    stackName: config.stackName,
+    workDir: config.cwd,
+  });
+
+  core.startGroup(config.stackName);
+
+  const onOutput = (msg: string) => {
+    core.info(msg);
+  }
+
+  const actions: Record<Commands, () => Promise<unknown>> = {
+    up() { return stack.up({ onOutput }) },
+    refresh() { return stack.refresh({ onOutput }) },
+    destroy() { return stack.destroy({ onOutput }) },
+    async preview() {
+      const preview = await stack.up();
+      onOutput(preview.stdout);
+      return preview;
+    },
+  };
+
+  await actions[config.command]();
+
+  core.endGroup();
 })();
