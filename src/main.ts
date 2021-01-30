edbe1bec0979a37a52d7b1cb801e014d1f02a23d
Simen A. W. Olsen <so@bjerk.io>
2021-01-30 22:13:01 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import * as pulumiCli from './libs/pulumi-cli';
 import { invariant } from './libs/utils';
 import { LocalWorkspace } from '@pulumi/pulumi/x/automation';
 import * as core from '@actions/core';
-import { exec } from './libs/exec';
 import { resolve } from 'path';
 import { environmentVariables } from './libs/envs';
 
@@ -17,12 +16,6 @@ const main = async () => {
 
   const workDir = resolve(environmentVariables.GITHUB_WORKSPACE, config.cwd);
 
-  const res = await exec('ls -l');
-  core.debug(res.stdout);
-
-  const res2 = await exec(`ls -l ${workDir}`);
-  core.debug(res2.stdout);
-
   // invariant(
   //   await fs.access(workDir),
   //   `Could not access working directory: ${workDir}`,
@@ -30,13 +23,10 @@ const main = async () => {
 
   const stack = await LocalWorkspace.selectStack({
     stackName: config.stackName,
-    workDir: config.cwd,
+    workDir: workDir,
   });
 
-  core.debug(JSON.stringify(stack, null, 2));
-  core.debug(`startGrouping: ${config.stackName}`);
-  core.startGroup(config.stackName);
-  core.debug(`end startGroup: ${config.stackName}`);
+  core.startGroup(`pulumi ${config.command} on ${config.stackName}`);
 
   const onOutput = (msg: string) => {
     core.debug(msg);
@@ -48,11 +38,9 @@ const main = async () => {
     refresh: () => stack.refresh({ onOutput }),
     destroy: () => stack.destroy({ onOutput }),
     preview: async () => {
-      const preview = await stack.up();
-      onOutput(preview.stdout);
-      onOutput(preview.stderr);
-      core.debug(JSON.stringify(preview.outputs, null, 2));
-      return preview;
+      const preview = await stack.preview();
+      preview.stdout && onOutput(preview.stdout);
+      preview.stderr && onOutput(preview.stderr);
     },
   };
 
