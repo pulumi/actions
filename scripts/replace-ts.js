04ad8d00231415e71f7f54e81b92e8ff9ec15d37
Simen A. W. Olsen <so@bjerk.io>
2021-02-03 00:27:02 +0100

    add reference to issue

--- scripts/replace-ts.js
+++ scripts/replace-ts.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// See https://github.com/vercel/ncc/issues/642
+
 let content = fs.readFileSync('dist/index.js', 'utf-8');
 content = content.replace(/require\("typescript"\)/g, '{}');
 fs.writeFileSync('dist/index.js', content);
