14a0d31203b14597231fe9a54d691964d401f621
Simen A. W. Olsen <so@bjerk.io>
2021-02-08 20:40:49 +0100

    refactor: Improve invariant (#60)

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,10 +1,9 @@
 /* eslint @typescript-eslint/explicit-module-boundary-types: 0 */
-/* eslint @typescript-eslint/no-explicit-any: 0 */
-import * as core from '@actions/core';
-export function invariant(condition: any, message?: string): asserts condition {
+export function invariant(
+  condition: unknown,
+  message?: string,
+): asserts condition {
   if (!condition) {
-    core.setFailed(message);
-    process.exit();
-    // throw new Error();
+    throw new Error(message);
   }
 }
