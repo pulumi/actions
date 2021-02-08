b1f9d35d02888fbbb44a55596b4b01c04d3291d6
Simen A. W. Olsen <so@bjerk.io>
2021-02-08 22:18:46 +0100

    fix: `commentOnPr` always true

---
+++ src/__tests__/config.test.ts
@@ -0,0 +1,75 @@
+const defaultConfig: Record<string, string> = {
+  command: 'up',
+  'stack-name': 'dev',
+  'work-dir': './',
+  'cloud-url': 'file://~',
+  'github-token': 'n/a',
+};
+
+describe('config.ts', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+  it('should validate a configuration', async () => {
+    const config = {
+      ...defaultConfig,
+      'comment-on-pr': 'false',
+    };
+    jest.mock('@actions/core', () => ({
+      getInput: jest.fn((name: string) => {
+        return config[name];
+      }),
+    }));
+
+    const { makeConfig } = require('../config');
+
+    const c = await makeConfig();
+    expect(c).toBeTruthy();
+    expect(c).toEqual({
+      command: 'up',
+      stackName: 'dev',
+      cloudUrl: 'file://~',
+      githubToken: 'n/a',
+      commentOnPr: false,
+      workDir: './',
+    });
+  });
+  it('should fail if configuration are invalid', async () => {
+    const config: Record<string, string> = {
+      command: 'sideways',
+    };
+    jest.mock('@actions/core', () => ({
+      getInput: jest.fn((name: string) => {
+        return config[name];
+      }),
+    }));
+
+    const { makeConfig } = require('../config');
+
+    await expect(makeConfig()).rejects.toThrow();
+  });
+  it('should validate a configuration with commentOnPr eq true', async () => {
+    const config = {
+      ...defaultConfig,
+      'comment-on-pr': 'true',
+    };
+    jest.mock('@actions/core', () => ({
+      getInput: jest.fn((name: string) => {
+        return config[name];
+      }),
+    }));
+
+    const { makeConfig } = require('../config');
+
+    const c = await makeConfig();
+    expect(c).toBeTruthy();
+    expect(c).toEqual({
+      command: 'up',
+      stackName: 'dev',
+      cloudUrl: 'file://~',
+      githubToken: 'n/a',
+      commentOnPr: true,
+      workDir: './',
+    });
+  });
+});
