c58ee0a86392faf5425d1d0e845a13cd3648e338
Simen A. W. Olsen <so@bjerk.io>
2021-02-08 21:43:45 +0100

    test: Add tests (#61)

---
+++ src/libs/__mocks__/@actions/exec.ts
@@ -0,0 +1,11 @@
+import { ExecOptions } from '@actions/exec';
+
+export const exec = jest.fn(
+  (commandLine: string, _?: string[], options?: ExecOptions) => {
+    if (commandLine === 'succeed') {
+      options.listeners.stdout(Buffer.from('hello world'));
+    } else {
+      options.listeners.stderr(Buffer.from('hello world'));
+    }
+  },
+);
---
+++ src/libs/__tests__/pr.test.ts
@@ -0,0 +1,41 @@
+import * as gh from '@actions/github';
+import { addPullRequestMessage } from '../pr';
+
+const createComment = jest.fn();
+jest.mock('@actions/github', () => ({
+  context: {},
+  getOctokit: jest.fn(() => ({
+    issues: {
+      createComment,
+    },
+  })),
+}));
+
+describe('pr.ts', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+  it('should add pull request message', async () => {
+    // @ts-ignore
+    gh.context = {
+      payload: {
+        pull_request: {
+          number: 123,
+        },
+      },
+    };
+
+    process.env.GITHUB_REPOSITORY = 'pulumi/actions';
+
+    await addPullRequestMessage('test', 'test');
+    expect(createComment).toHaveBeenCalled();
+  });
+  it('should fail if no pull request data', async () => {
+    process.env.GITHUB_REPOSITORY = 'pulumi/actions';
+    // @ts-ignore
+    gh.context = { payload: {} };
+    await expect(addPullRequestMessage('test', 'test')).rejects.toThrowError(
+      'Missing pull request event data',
+    );
+  });
+});
