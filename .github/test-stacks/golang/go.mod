bcc6a3c0d30854660a538cf1d177067172331b2c
Paul Stack <public@paulstack.co.uk>
2021-02-25 12:04:41 +0000

    Starting to build a CI matrix for the different action workflows (#82)

--- .github/test-stacks/golang/go.mod
+++ .github/test-stacks/golang/go.mod
@@ -3,7 +3,6 @@ module golang
 go 1.14
 
 require (
-	github.com/pulumi/pulumi-random/sdk/v2 v2.5.0
-	github.com/pulumi/pulumi-random/sdk/v3 v3.0.1 // indirect
-	github.com/pulumi/pulumi/sdk/v2 v2.14.0
+	github.com/pulumi/pulumi-random/sdk/v3 v3.0.1
+	github.com/pulumi/pulumi/sdk/v2 v2.21.0
 )
--- .github/test-stacks/golang/main.go
+++ .github/test-stacks/golang/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	random "github.com/pulumi/pulumi-random/sdk/v2/go/random"
+	random "github.com/pulumi/pulumi-random/sdk/v3/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		p, err := random.NewRandomPet(ctx, "my-user-name", &random.RandomPetArgs{})
+		s, err := random.NewRandomString(ctx, "my-user-name", &random.RandomStringArgs{
+			Length: pulumi.Int(60),
+		})
 		if err != nil {
 			return err
 		}
-		ctx.Export("pet-name", p)
+		ctx.Export("name", s.Result)
 		return nil
 	})
 }
