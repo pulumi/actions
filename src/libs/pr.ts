be20a58d32ae474e9b5ae515e3a82fa406723065
Graham Plata <graham.plata@gmail.com>
2021-03-31 10:52:59 -0400

    further cleanup

--- src/libs/pr.ts
+++ src/libs/pr.ts
@@ -6,45 +6,29 @@ export async function handlePullRequestMessage(
   body: string,
   githubToken: string,
 ): Promise<void> {
-  invariant(context.payload.pull_request, 'Missing pull request event data.');
+  const { payload, repo } = context;
+  invariant(payload.pull_request, 'Missing pull request event data.');
+  const text = `#### :tropical_drink: `
+  const octokit = getOctokit(githubToken);
+  const comments = await octokit.issues.listComments({
+    ...repo,
+    issue_number: payload.pull_request.number
+  })
+  const comment_id = comments.data.find(comment => comment.body.startsWith(text));
 
   if (body && githubToken) {
-    if (context.payload.pull_request.number) {
-      await createCommentOnPullRequest(body, githubToken);
+    if (comment_id) {
+      await octokit.issues.updateComment({
+        ...repo,
+        comment_id: comment_id.id,
+        body,
+      });
+    } else {
+      await octokit.issues.createComment({
+        ...repo,
+        issue_number: payload.pull_request.number,
+        body,
+      });
     }
   }
-}
-
-async function createCommentOnPullRequest(body, githubToken) {
-  const commentId = await findPreviousComments(`#### :tropical_drink: `, githubToken);
-  const octokit = getOctokit(githubToken);
-
-  if (commentId) {
-    await octokit.issues.updateComment({
-      ...context.repo,
-      comment_id: commentId,
-      body,
-    });
-  } else {
-    await octokit.issues.createComment({
-      ...context.repo,
-      issue_number: context.payload.pull_request.number,
-      body,
-    });
-  }
-}
-
-async function getComments(githubToken) {
-  const octokit = getOctokit(githubToken);
-
-  return octokit.issues.listComments({
-    ...context.repo,
-    issue_number: context.payload.pull_request.number
-  });
-}
-
-async function findPreviousComments(text, githubToken) {
-  const { data: comments } = await getComments(githubToken);
-  const previousComment = comments.find(comment => comment.body.startsWith(text));
-  return (previousComment ? previousComment.id : null);
 }
