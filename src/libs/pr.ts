7266276358046c9be56d46169538c5ef4285ab45
Graham Plata <graham.plata@gmail.com>
2021-03-31 09:11:54 -0400

    make pr changes

--- src/libs/pr.ts
+++ src/libs/pr.ts
@@ -1,25 +1,23 @@
-// import * as core from '@actions/core';
 import { context, getOctokit } from '@actions/github';
 import { invariant } from './utils';
 
-let octokit;
 
 export async function handlePullRequestMessage(
   body: string,
   githubToken: string,
 ): Promise<void> {
   invariant(context.payload.pull_request, 'Missing pull request event data.');
-  octokit = getOctokit(githubToken);
 
   if (body && githubToken) {
     if (context.payload.pull_request.number) {
-      await createCommentOnPullRequest(body);
+      await createCommentOnPullRequest(body, githubToken);
     }
   }
 }
 
-async function createCommentOnPullRequest(body) {
-  const commentId = await findPreviousComments(`#### :tropical_drink: `);
+async function createCommentOnPullRequest(body, githubToken) {
+  const commentId = await findPreviousComments(`#### :tropical_drink: `, githubToken);
+  const octokit = getOctokit(githubToken);
 
   if (commentId) {
     await octokit.issues.updateComment({
@@ -36,15 +34,17 @@ async function createCommentOnPullRequest(body) {
   }
 }
 
-async function getComments() {
+async function getComments(githubToken) {
+  const octokit = getOctokit(githubToken);
+
   return octokit.issues.listComments({
     ...context.repo,
     issue_number: context.payload.pull_request.number
   });
 }
 
-async function findPreviousComments(text) {
-  const { data: comments } = await getComments();
+async function findPreviousComments(text, githubToken) {
+  const { data: comments } = await getComments(githubToken);
   const previousComment = comments.find(comment => comment.body.startsWith(text));
   return (previousComment ? previousComment.id : null);
 }
