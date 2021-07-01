85b316bcfb36e1352b9b2af48c8fc5cee141f1fa
Simen A. W. Olsen <so@bjerk.io>
2021-07-01 13:37:19 +0200

    wip

--- src/libs/pr.ts
+++ src/libs/pr.ts
@@ -1,35 +1,55 @@
+import * as core from '@actions/core';
 import { context, getOctokit } from '@actions/github';
+import * as dedent from 'dedent';
+import { Config } from '../config';
 import { invariant } from './utils';
 
-
 export async function handlePullRequestMessage(
-  body: string,
-  githubToken: string,
-  command: string,
-  stackName: string,
-  editCommentOnPr?: boolean,
+  config: Config,
+  output: string,
 ): Promise<void> {
+  const {
+    githubToken,
+    command,
+    stackName,
+    options: { editCommentOnPr },
+  } = config;
+
+  const heading = `#### :tropical_drink: \`${command}\` on ${stackName}`;
+  const body = dedent`
+    ${heading}
+    \`\`\`
+    ${output}
+    \`\`\`
+  `;
+
   const { payload, repo } = context;
   invariant(payload.pull_request, 'Missing pull request event data.');
-  const text = `#### :tropical_drink: \`${command}\` on ${stackName}`
+
   const octokit = getOctokit(githubToken);
 
-  if (editCommentOnPr) {
-    const { data: comments} = await octokit.rest.issues.listComments({
-      ...repo,
-      issue_number: payload.pull_request.number
-    })
-    const comment = comments.find(comment => comment.body.startsWith(text));
-
-    // If comment exists, update it.
-    if (comment) {
-      await octokit.rest.issues.updateComment({
+  try {
+    if (editCommentOnPr) {
+      const { data: comments } = await octokit.rest.issues.listComments({
         ...repo,
-        comment_id: comment.id,
-        body,
+        issue_number: payload.pull_request.number,
       });
-      return;
+      const comment = comments.find((comment) =>
+        comment.body.startsWith(heading),
+      );
+
+      // If comment exists, update it.
+      if (comment) {
+        await octokit.rest.issues.updateComment({
+          ...repo,
+          comment_id: comment.id,
+          body,
+        });
+        return;
+      }
     }
+  } catch {
+    core.warning('Not able to edit comment, defaulting to creating a new comment.');
   }
 
   await octokit.rest.issues.createComment({
