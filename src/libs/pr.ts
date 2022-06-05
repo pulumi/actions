94a61b4e0b3e7ff83212dd5f1ce82f89f67c39e1
John Brunton <john_brunton@live.co.uk>
2022-06-05 20:39:08 +0100

    fix: update PR comment

--- src/libs/pr.ts
+++ src/libs/pr.ts
@@ -15,16 +15,17 @@ export async function handlePullRequestMessage(
     options: { editCommentOnPr },
   } = config;
 
-  const heading = `#### :tropical_drink: \`${command}\` on ${stackName}
-
-  <details>
-  <summary>Click to expand Pulumi report</summary>`;
+  const heading = `#### :tropical_drink: \`${command}\` on ${stackName}`;
+  const summary = '<summary>Click to expand Pulumi report</summary>';
 
   const rawBody = output.substring(0, 64_000);
   // a line break between heading and rawBody is needed
   // otherwise the backticks won't work as intended
   const body = dedent`
     ${heading}
+
+    <details>
+    ${summary}
 
     \`\`\`
     ${rawBody}
@@ -49,7 +50,7 @@ export async function handlePullRequestMessage(
         issue_number: payload.pull_request.number,
       });
       const comment = comments.find((comment) =>
-        comment.body.startsWith(heading),
+        comment.body.startsWith(heading) && comment.body.includes(summary),
       );
 
       // If comment exists, update it.
