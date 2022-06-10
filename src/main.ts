88bbf6616ba37221b874f6a32d28bfc47e20dd2f
Nikita Sharaev <sharaev.n@n-t.io>
2022-06-10 09:32:13 +0400

    # This is a combination of 3 commits. # This is the 1st commit message:
    Add scheme
    
    # This is the commit message #2:
    
    Test errors
    
    # This is the commit message #3:
    
    chore(dist): Update dist [automated commit]

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { resolve } from 'path';
 import * as core from '@actions/core';
 import {
+  ConfigMap,
   LocalProgramArgs,
   LocalWorkspace,
   LocalWorkspaceOptions,
-  ConfigMap,
 } from '@pulumi/pulumi/automation';
+import YAML from 'yaml';
 import { Commands, makeConfig } from './config';
 import { environmentVariables } from './libs/envs';
 import { handlePullRequestMessage } from './libs/pr';
 import * as pulumiCli from './libs/pulumi-cli';
 import { invariant } from './libs/utils';
-import YAML from 'yaml';
 
 const pulumiVersion = '^3';
 
