36a19d2d13318d23335eaa56225a63898940741c
Robbie McKinstry <thesnowmancometh@gmail.com>
2022-07-07 17:05:11 -0400

    Fix typo in variable name.

--- src/main.ts
+++ src/main.ts
@@ -91,7 +91,7 @@ const main = async () => {
     }
   }
 
-  if (config.commentOnPr && config.is_pull_request) {
+  if (config.commentOnPr && config.isPullRequest) {
     core.debug(`Commenting on pull request`);
     invariant(config.githubToken, 'github-token is missing.');
     handlePullRequestMessage(config, output);
