6708551af7e19e64f635c7f9ef324be5c3c92a47
Jonathan Tooker <jonathan.tooker@stackpath.com>
2023-02-08 20:49:16 -0600

    Add a summary of changes to the PR comment

--- src/libs/pr.ts
+++ src/libs/pr.ts
@@ -21,11 +21,35 @@ export async function handlePullRequestMessage(
   const summary = '<summary>Pulumi report</summary>';
 
   const rawBody = output.substring(0, 64_000);
+
+  // Make sure the output isn't empty. If it's not, get the last 100 lines.
+  const last100Lines = output.length >= 0 ? output.slice(-100) : '';
+
+  const diffChanges = last100Lines.match(/\~[0-9]+ to update/);
+  const diffCreations = last100Lines.match(/\+[0-9]+ to create/);
+  const diffDeletions = last100Lines.match(/\-[0-9]+ to delete/);
+  const diffUnchanged = last100Lines.match(/[0-9]+ unchanged/);
+
+  // Regex to match the diff summary
+  const searchPattern =
+    /(~|\+|-)?[0-9]+ (to)? (update|create|delete|unchanged)/;
+
+  // Generate a summary with the changes, if any. If there's none, return an empty string.
+  // If certain diffs are absent, they're set to 0 to make output consistent
+  const diffSummary = searchPattern.test(last100Lines)
+    ? `:red_circle: ${diffDeletions ?? '-0 to delete'},
+       :yellow_circle: ${diffChanges ?? '~0 to update'},
+       :green_circle: ${diffCreations ?? '+0 to create'},
+       :white_circle: ${diffUnchanged ?? '0 unchanged'}
+      `
+    : '';
+
   // a line break between heading and rawBody is needed
   // otherwise the backticks won't work as intended
   const body = dedent`
     ${heading}
 
+    ${diffSummary}
     <details>
     ${summary}
 
