975d5bf81e00d7324867d8ee880b402ea4f447c7
Simen A. W. Olsen <so@bjerk.io>
2023-04-11 21:49:29 +0200

    use configmap type

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import { ConfigMap } from '@pulumi/pulumi/automation';
 import {
   getBooleanInput,
   getInput,
@@ -48,7 +49,7 @@ export function makeConfig() {
     // TODO: Add parser back once pulumi/pulumi#12641 is fixed.
     // @see https://github.com/pulumi/actions/pull/913
     // @see https://github.com/pulumi/actions/pull/912
-    configMap: getYAMLInput<string>('config-map'),
+    configMap: getYAMLInput<ConfigMap>('config-map'),
     editCommentOnPr: getBooleanInput('edit-pr-comment'),
 
     options: {
