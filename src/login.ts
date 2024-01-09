2cbbbbf73716d3938144db8a52d17ec29da36dc8
Thomas Gummerer <t.gummerer@gmail.com>
2024-01-09 01:16:00 +0100

    clean up login code

--- src/login.ts
+++ src/login.ts
@@ -2,13 +2,11 @@ import * as core from '@actions/core';
 import * as exec from './libs/exec';
 import * as pulumiCli from './libs/pulumi-cli';
 
-export const login = async (cloudUrl: string, accessToken: string): Promise<exec.ExecResult> => {
+export const login = async (cloudUrl: string): Promise<exec.ExecResult> => {
   if (cloudUrl) {
     core.info(`Logging into ${cloudUrl}`);
     return pulumiCli.run('--non-interactive' ,'login', cloudUrl);
-  } else if (accessToken !== '') {
-    core.info("Logging into the Pulumi Cloud backend.");
-    return pulumiCli.run('--non-interactive', 'login');
   }
-  return Promise.resolve({ success: true, stdout: '', stderr: '' });
+  core.info("Logging into the Pulumi Cloud backend.");
+  return pulumiCli.run('--non-interactive', 'login');
 };
