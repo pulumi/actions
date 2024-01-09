7be0c6ac189fe51adb8ec8ee8a6c4e61fb9adca2
Thomas Gummerer <t.gummerer@gmail.com>
2024-01-09 01:55:56 +0100

    fixup tests; clean up environment variables (#1063)
    * clean up environment variables
    
    * fixup tests

--- src/libs/envs.ts
+++ src/libs/envs.ts
@@ -2,9 +2,6 @@ import * as envalid from 'envalid';
 
 export const environmentVariables = envalid.cleanEnv(process.env, {
   GITHUB_WORKSPACE: envalid.str(),
-  PULUMI_ACCESS_TOKEN: envalid.str({
-    default: '',
-  }),
 });
 
 export type EnvironmentVariables = typeof environmentVariables;
