c6c2b8b50f3fd56b0e606cb110777134c46ff4c9
Thomas Gummerer <t.gummerer@gmail.com>
2024-01-09 01:24:08 +0100

    fixup tests

--- src/__tests__/run.test.ts
+++ src/__tests__/run.test.ts
@@ -55,25 +55,13 @@ describe('main.login', () => {
   });
   it('should read self-hosted backend config', async () => {
     const cloudUrl = 's3://my_region.aws.com';
-    await login(cloudUrl, '');
+    await login(cloudUrl);
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith('--non-interactive', 'login', cloudUrl);
   });
-  it('should login when an access token is provided', async () => {
-    const accessToken = 'my_access_token';
-    await login('', accessToken);
+  it('should login when no cloud url is provided', async () => {
+    await login('');
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith('--non-interactive', 'login');
-  });
-  it('should not login when nothing provided', async () => {
-    await login('', '');
-    expect(spy).not.toHaveBeenCalled();
-  });
-  it('should login with cloud url when both are specified', async () => {
-    const cloudLogin = 'my login url';
-    const accessToken = 'my access token';
-    await login(cloudLogin, accessToken);
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenCalledWith('--non-interactive', 'login', cloudLogin);
   });
 });
