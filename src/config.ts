2d4ff9594eba943caf464953cb8ecbc6f9c87d26
Thomas Gummerer <t.gummerer@gmail.com>
2024-01-11 01:58:49 +0100

    fix user agent

--- src/config.ts
+++ src/config.ts
@@ -65,7 +65,7 @@ export function makeConfig() {
       policyPackConfigs: parseSemicolorToArray(
         getMultilineInput('policyPackConfigs'),
       ),
-      userAgent: 'pulumi/actions@v3',
+      userAgent: 'pulumi/actions@v5',
       color: getUnionInput('color', {
         alternatives: ['always', 'never', 'raw', 'auto'] as const,
       }),
