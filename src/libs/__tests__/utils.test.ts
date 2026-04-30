22d32ee5655468af3f635c563c50250872b4f5e7
Nelson Figueroa <30811275+nelsonfigueroa@users.noreply.github.com>
2026-04-30 14:34:39 -0700

    chore: update ESLint stack and migrate to flat config (#1406)
    * chore: update ESLint stack and migrate to flat config
    
    * update changelog

--- src/libs/__tests__/utils.test.ts
+++ src/libs/__tests__/utils.test.ts
@@ -2,17 +2,13 @@ import { parseSemicolorToArray, stripAnsiControlCodes } from '../utils';
 
 describe('utils.ts', () => {
   it('should parse semicolon to array', () => {
-
     const inputArray = parseSemicolorToArray(['hello,world']);
 
     expect(inputArray[0]).toBe('hello');
     expect(inputArray[1]).toBe('world');
   });
-});
 
-describe('utils.ts', () => {
-  it('should parse semicolon to array', () => {
-
+  it('should strip ansi control codes', () => {
     const output = stripAnsiControlCodes('\x1b[30mblack\x1b[37mwhite');
 
     expect(output).toBe('blackwhite');
